e8583de0ce49fe03898d0cebfce9e362467d018d
Alaa33ib <alaa33ib@gmail.com>
2026-02-22 15:49:04 +0300

    Reorganized files
    - Deleted page1 replaced it with quiz page
    - Deleted empty pages
    - Merged main and welcome page will rename it -> home

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
+
 import streamlit as st
+import Background.background as background
 
-name = st.text_input("Please enter your name:")
+st.set_page_config(page_title="Welcome", layout="centered")
 
-if name:
-    st.session_state.name = name
+def discription():
+    st.header("Quiz discription:")
+    st.write("This quiz is designed to check what personality you are, you may choose the theme of the quiz, and answer the questions, at the end you will get your personality type and a description about it.")
+    st.write("Please choose an option below:")
 
-if st.button("Ok"):
-    st.switch_page("pages/welcomPage.py")
+if "name" not in st.session_state:
+    st.session_state.name = None
+
+if st.session_state.name is None:
+    st.write("Hello There👋")
+    name = st.text_input("Please enter your name:")
+    if st.button("Ok"):
+        if name:
+            st.session_state.name = name
+            st.rerun() # Refresh to show the menu below
+else:
+    st.write(f"Hello, {st.session_state.name} 👋")
+    discription()
+    col1, col2, col3 = st.columns([1,2,1])
+
+    with col2:
+        c1, c2 = st.columns(2)
+
+        with c1:
+            if st.button("Detective Conan Quiz"):
+                st.switch_page("pages/quiz.py")
+
+        with c2:
+            if st.button("Page 2"):
+                st.switch_page("pages/page2.py")
--- pages/page1.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Page 1")
-
-st.write("This is Page 1")
-
-if st.button("Back to Home"):
-    st.switch_page("pages/welcomPage.py")
